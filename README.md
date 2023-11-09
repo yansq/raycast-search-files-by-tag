a871b0e68c6c4539ab83af1f9a1f881f06a345a2
Daniel Yan <felzyan33@gmail.com>
2023-11-09 22:22:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# <p align="center"> Search Files By Tag</p>
+
+<p align="center">A Raycast extension to search files by tag</p>
+
+## Installation
+
+- Make sure you have [Raycast](https://www.raycast.com/), [Node.js](https://nodejs.org/en) and [npm](https://www.npmjs.com/package/npm) installed first,
+- Clone this repo,
+- Run `npm i`,
+- Run `Import Extension` command in Raycast, and select the folder.
---
+++ src/index.tsx
@@ -0,0 +1,46 @@
+import { ActionPanel, List, Action } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { useExec } from "@raycast/utils";
+import { showInFinder } from "@raycast/api";
+import fs from "node:fs";
+
+export default function Command() {
+  const [searchTag, setSearchTag] = useState("");
+  const [files, setFiles] = useState([]);
+  const { isLoading, data } = useExec(`mdfind 'tag:${searchTag === "" ? "ZZZZZZ" : searchTag}'`, {shell: true, keepPreviousData: true});
+
+  useEffect(() => {
+    if (data) {
+      setFiles(data.split(/\r?\n/));
+    }
+  }, [data]);
+  
+  const getMetaData = (id: string | null) => {
+    if (id) {
+      fs.stat(id, (err, stat) => {
+        console.log(stat)
+      })
+    }
+  }
+
+  return (
+    <List 
+      filtering={false}
+      onSearchTextChange={setSearchTag}
+      onSelectionChange={getMetaData}
+    >
+      {files.map((item) => (
+        <List.Item
+          key={item}
+          id={item}
+          title={item}
+          actions={
+            <ActionPanel>
+              <Action title="Show in Finder" onAction={() => showInFinder(item)} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
