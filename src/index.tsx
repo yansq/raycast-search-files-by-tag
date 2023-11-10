93eea36919f8e1a348401e9f74641317e4de693e
Daniel Yan <felzyan33@gmail.com>
2023-11-10 09:48:03 +0800

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,15 @@ import { ActionPanel, List, Action } from "@raycast/api";
 import { useEffect, useState } from "react";
 import { useExec } from "@raycast/utils";
 import { showInFinder } from "@raycast/api";
-//import fs from "node:fs";
 
 export default function Command() {
   const [searchContent, setSearchContent] = useState(["", ""]);
-  const [files, setFiles] = useState([]);
-  const { isLoading, data } = useExec(`mdfind 'tag:${searchContent[0] === "" ? "ZZZZZZ" : searchContent[0]}'`, {shell: true, keepPreviousData: true});
+  const [files, setFiles] = useState<string[]>([]);
+  const { data } = useExec(`mdfind 'tag:${searchContent[0] === "" ? "ZZZZZZ" : searchContent[0]}'`, {shell: true, keepPreviousData: true});
 
   useEffect(() => {
     if (data) {
-      let files = data.split(/\r?\n/)
+      let files = data.split(/\r?\n/);
       if (searchContent[1]) {
         files = files.filter((item) => item.includes(searchContent[1]));
       }
@@ -20,24 +19,14 @@ export default function Command() {
   }, [data, searchContent[1]]);
 
   const updateSearch = (str: string) => {
-    const newContent = [str.split(" ")[0], str.split(" ")[1]];
-    console.log(newContent);
-    setSearchContent(newContent);
+    const [tag, content] = str.split(" ");
+    setSearchContent([tag, content]);
   }
-  
-  //const getMetaData = (id: string | null) => {
-    //if (id) {
-      //fs.stat(id, (err, stat) => {
-        //console.log(stat)
-      //})
-    //}
-  //}
 
   return (
     <List 
       filtering={false}
       onSearchTextChange={updateSearch}
-      //onSelectionChange={getMetaData}
     >
       {files.map((item) => (
         <List.Item
