76338682e8bd66e1988e5ce73de865c160956739
Daniel Yan <felzyan33@gmail.com>
2025-05-07 18:03:44 +0800

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,30 @@
 import { ActionPanel, List, Action, useNavigation } from "@raycast/api";
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { useExec } from "@raycast/utils";
 import { showInFinder } from "@raycast/api";
 
 export default function Command() {
-  const [searchContent, setSearchContent] = useState(["", ""]);
-  const { data } = useExec(`mdfind 'tag:${searchContent[0] === "" ? "ZZZZZZ" : searchContent[0]}'`, {
-    shell: true,
-    keepPreviousData: true,
-  });
   const { pop } = useNavigation();
+  const [[tag, content], setSearchContent] = useState(["", ""]);
+  const { data } = useExec("mdfind", [tag && `tag:${tag}`], {
+    shell: true,
+    execute: !!tag,
+  });
 
-  const files: string[] = useMemo(() => {
-    if (data) {
-      let files = data.split(/\r?\n/);
-      if (searchContent[1]) {
-        files = files.filter((item) => item.includes(searchContent[1]));
-      }
-      return files;
-    } else {
-      return [];
-    }
-  }, [data, searchContent[1]]);
+  let files = data?.split(/\r?\n/);
+  files = tag && content ? files?.filter((item) => item.includes(content)) : files;
 
   const updateSearch = (str: string) => {
-    const [tag, content] = str.split(" ");
+    if (str.trim() === "") {
+      return;
+    }
+    const [tag, content] = str.split(/ (.*)/s);
     setSearchContent([tag, content]);
   };
 
   return (
     <List filtering={false} onSearchTextChange={updateSearch}>
-      {files.map((item) => (
+      {files?.map((item) => (
         <List.Item
           key={item}
           id={item}
