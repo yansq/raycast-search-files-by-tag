01c02b2b9535b19b905a75554049be1c4e3d03c0
Daniel Yan <felzyan33@gmail.com>
2025-05-07 16:56:48 +0800

    refactor: use useMemo instead of useEffect

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,35 @@
 import { ActionPanel, List, Action, useNavigation } from "@raycast/api";
-import { useEffect, useState } from "react";
+import { useState, useMemo } from "react";
 import { useExec } from "@raycast/utils";
 import { showInFinder } from "@raycast/api";
 
 export default function Command() {
   const [searchContent, setSearchContent] = useState(["", ""]);
-  const [files, setFiles] = useState<string[]>([]);
-  const { data } = useExec(`mdfind 'tag:${searchContent[0] === "" ? "ZZZZZZ" : searchContent[0]}'`, { shell: true, keepPreviousData: true });
+  const { data } = useExec(`mdfind 'tag:${searchContent[0] === "" ? "ZZZZZZ" : searchContent[0]}'`, {
+    shell: true,
+    keepPreviousData: true,
+  });
   const { pop } = useNavigation();
 
-  useEffect(() => {
+  const files: string[] = useMemo(() => {
     if (data) {
       let files = data.split(/\r?\n/);
       if (searchContent[1]) {
         files = files.filter((item) => item.includes(searchContent[1]));
       }
-      setFiles(files);
+      return files;
+    } else {
+      return [];
     }
   }, [data, searchContent[1]]);
 
   const updateSearch = (str: string) => {
     const [tag, content] = str.split(" ");
     setSearchContent([tag, content]);
-  }
+  };
 
   return (
-    <List
-      filtering={false}
-      onSearchTextChange={updateSearch}
-    >
+    <List filtering={false} onSearchTextChange={updateSearch}>
       {files.map((item) => (
         <List.Item
           key={item}
@@ -36,12 +37,13 @@ export default function Command() {
           title={item}
           actions={
             <ActionPanel>
-              <Action title="Show in Finder" onAction={
-                () => {
+              <Action
+                title="Show in Finder"
+                onAction={() => {
                   showInFinder(item);
                   pop();
-                }
-              } />
+                }}
+              />
             </ActionPanel>
           }
         />
